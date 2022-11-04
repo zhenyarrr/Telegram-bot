c8db8745fbd85abbc57320c66d60dc468a1a7dc5
Evgenia Rakushina <98237518+zhenyarrr@users.noreply.github.com>
2022-11-04 13:52:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def help(message: telebot.types.Message):
 def values(message: telebot.types.Message):
     text = 'доступные валюты:'
     for key in keys.keys():
-        text = '\n'.join((text, key, ))
+        text = '\n'.join((text, key,))
     bot.reply_to(message, text)
 
 
@@ -32,13 +32,14 @@ def convert(message: telebot.types.Message):
 
         quote, base, amount = values
         total_base = CryptoConvertor.convert(quote, base, amount)
+
     except ConvertionException as e:
         bot.reply_to(message, f'Ошибка пользователя.\n{e}')
     except Exception as e:
         bot.reply_to(message, f'Не удалось обработать команду\n{e}')
 
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        text = f'Цена {amount} {quote} в {base} - {total_base * float(amount)}'
         bot.send_message(message.chat.id, text)
 
 bot.polling()
