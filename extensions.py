09dd3bdd059be5dd07109a9e628074cb5e625939
Evgenia Rakushina <98237518+zhenyarrr@users.noreply.github.com>
2022-11-04 13:56:34 +0300

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -28,6 +28,6 @@ class CryptoConvertor:
             raise ConvertionException(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[keys[base]]
+        total_base = float(json.loads(r.content)[keys[base]])
 
         return total_base
